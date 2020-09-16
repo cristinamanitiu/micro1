3a622ee81602f6cb73ac7696bd281714958d79b7
Cristina Manitiu <cristina.manitiu@softvision.ro>
2020-09-16 15:18:58 +0300

    minor changes

--- src/main/java/com/example/micro1/SnsSubscriber.java
+++ src/main/java/com/example/micro1/SnsSubscriber.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.sns.AmazonSNS;
 import com.amazonaws.services.sns.model.CreateTopicResult;
 import com.amazonaws.services.sns.model.SubscribeRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +15,15 @@ public class SnsSubscriber {
     @Autowired
     AmazonSNS amazonSNS;
 
+    @Value("${micro1.endpoint}")
+    private String endpoint;
+
     private final String SNS_TOPIC_NAME="my-topic";
 
     @PostConstruct
     public void registerToTopic(){
         CreateTopicResult topic = amazonSNS.createTopic(SNS_TOPIC_NAME);
-        SubscribeRequest request = new SubscribeRequest(topic.getTopicArn(),"http","http://micro1:8082/test1");
+        SubscribeRequest request = new SubscribeRequest(topic.getTopicArn(),"http",endpoint + "/test1");
         amazonSNS.subscribe(request);
     }
 
